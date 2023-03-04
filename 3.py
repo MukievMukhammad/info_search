ce69b94d65f984445de50ced8c221256448d9525
MukievMukhammad <42972450+MukievMukhammad@users.noreply.github.com>
2023-03-04 12:17:36 +0300

    sp

--- 3.py
+++ 3.py
@@ -4,24 +4,28 @@ def find_docs_contains(word):
     with open('inverse_idx.json') as json_file:
         inverse_idx = json.load(json_file)
     result = []
-    if word.startswith('!'):
-        return [page for k,v in inverse_idx if k != word[1:] for page in v ]
-    else:
-        return inverse_idx[word]
+    try:
+        if word.startswith('!'):
+            return [page for k,v in inverse_idx if k != word[1:] for page in v ]
+        else:
+            return inverse_idx[word]
+    except Exception as e:
+        print("Word not found: %s" % e)
 
 if __name__ == "__main__":
     result = []
     req = input()
-    sub_req_or = req.split('|', 'ИЛИ')
+    sub_req_or = req.split('|')
     for sub_or in sub_req_or:
         sub_or = sub_or.strip()
         if '&' in sub_or or 'И' in sub_or:
-            sub_req_and = sub_or.split('&', 'И')
+            sub_req_and = sub_or.split('&')
             intersect = set([])
             for idx, sub_and in enumerate(sub_req_and):
+                sub_and = sub_and.strip()
                 docs = find_docs_contains(word=sub_and)
                 if idx == 0:
-                    intersect |= docs
+                    intersect |= set(docs)
                 else:
                     intersect = set.intersection(intersect, docs)
             docs = list(intersect)
